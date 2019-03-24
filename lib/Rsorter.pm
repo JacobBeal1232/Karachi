5e7116d010c267113b14d8a53c99b2be5d536dfc
serafinjacob <48459490+serafinjacob@users.noreply.github.com>
2019-03-24 14:20:30 -0400

    Add files via upload

--- lib/Rsorter.pm
+++ lib/Rsorter.pm
@@ -57,7 +57,7 @@ foreach my $recordList ( @records ) {
     }
 }
 open (my $fileh, ">plot1.txt") or die "$!";
-print $fileh "Month,"."Category,"."CPI"."\n";
+print $fileh "Category,"."Month,"."CPI"."\n";
 #add up all CPI values
 for(my $k = 0; $k<=$recordCount; $k++){
 	for (my $n = 0; $n<12; $n++) {
@@ -159,7 +159,7 @@ for(my $m = 1; $m < 23; $m++){
 #print info to file for plotting
 for my $j (1..12){
 	for my $p (1..22) {
-			print $fileh "$refMonth[$p]".","."$total[$j]".","."$product[$p]"."\n";
+			print $fileh "$product[$p],"."$j,"."$total[$p]"."\n";
 			}
 	}
     close ($fileh);
@@ -168,8 +168,8 @@ for my $j (1..12){
 my $R = Statistics::R->new();
 
 # Set up the PDF file for plots
-$R->run(qq`pdf("Question1Plot.pdf" , paper="letter")`);
-
+$R->run(qq`pdf("Question1Plot.pdf", width=22, height =17 )`);
+#
 # Load the plotting library
 $R->run(q`library(ggplot2)`);
 
@@ -177,7 +177,7 @@ $R->run(q`library(ggplot2)`);
 $R->run(qq`data <- read.csv("plot1.txt")`);
 
 # plot the data as a line plot with each point outlined
-$R->run(q`ggplot(data, aes(x=Month, y=CPI, colour=Category, group=Category)) + geom_line() + geom_point(size=2) + labs(title = "Alcohol vs Education Monthly Trends", subtitle ="Average per Area 1985 - 2018 ") + ylab("Average CPI Value") + scale_x_continuous(breaks=c(0,1,2,3,4,5,6,7,8,9,10,11), labels=c("Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sept", "Oct", "Nov", "Dec"))`);
+$R->run(q`ggplot(data, aes(x=Month, y=CPI, colour=Category, group=Category)) + geom_line() + geom_point(size=2) + labs(title = "Alcohol vs Education Monthly Trends", subtitle ="Average per Area 1985 - 2018 ") + ylab("Average CPI Value") + xlab("Month") + scale_x_continuous(breaks=c(0,1,2,3,4,5,6,7,8,9,10,11,12), labels=c("","Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sept", "Oct", "Nov", "Dec"))`);
 # close down the PDF device
 $R->run(q`dev.off()`);
 
