da09a293e3aea782f76d559945da78fdcad32e09
serafinjacob <48459490+serafinjacob@users.noreply.github.com>
2019-03-25 02:42:34 -0400

    Add files via upload

---
+++ Final-Karachi-master/preprocessor/preprocess.pl
@@ -0,0 +1,338 @@
+#!/usr/bin/perl
+#
+#   Packages and modules
+#
+use strict;
+use warnings;
+use version;   our $VERSION = qv('5.16.0');   # This is the version of Perl to be used
+use Text::CSV  1.32;   # We will be using the CSV module (version 1.32 or higher)
+                       # to parse each line
+#
+#       preProcess.pl
+#      Date of Last Update: March 24, 2019.
+
+#      Commandline Parameters: 1
+#         $ARGV[0] = file to process
+#         $ARGV[1] = file type output
+#
+
+#
+#   Variables to be used
+#
+my $EMPTY = q{};
+my $SPACE = q{ };
+my $COMMA = q{,};
+
+my $filename     = $EMPTY;
+my @records;
+my $csv = Text::CSV->new({ sep_char => $COMMA });
+
+#
+#   Check that you have the right number of parameters
+#
+my $readFileName;
+my $questionType;
+if ($#ARGV != 1 ) {
+  print "Usage: preProcess.pl <filename> <questionnumber>\n" or
+    die "Print failure\n";
+  exit;
+} else {
+  $readFileName = $ARGV[0];
+  $questionType = $ARGV[1];
+}
+
+#
+#   Open the input file and load the contents into records array
+#
+open my $file, '<:encoding(UTF-8)', $readFileName
+  or die "Unable to open names file: $readFileName\n";
+@records = <$file>;
+close $file
+  or die "Unable to close: $readFileName\n";   # Close the input file
+
+#
+#   Field for question type
+#
+if($questionType == 1){
+  my @productDate;
+  my @productYear;
+  my @productAreaType;
+  my @productValue;
+  my $record_count = 0;
+  foreach my $cost_record(@records)
+  {
+    if ( $csv->parse($cost_record) )
+    {
+      my @master_fields = $csv->fields();
+      my @splitDate = split('-', $master_fields[0]);
+      if ($splitDate[0] > 1984 && $splitDate[0] < 2019)
+      {
+        if ($master_fields[1] eq "Canada" | $master_fields[1] eq "Newfoundland and Labrador" | $master_fields[1] eq "Prince Edward Island" | $master_fields[1] eq "Nova Scotia" | $master_fields[1] eq "New Brunswick" | $master_fields[1] eq "Quebec" | $master_fields[1] eq "Ontario" | $master_fields[1] eq "Manitoba" | $master_fields[1] eq "Saskatchewan" | $master_fields[1] eq "Alberta" | $master_fields[1] eq "British Columbia")
+        {
+          if ($master_fields[3] eq "Alcoholic beverages" | $master_fields[3] eq "Education and reading")
+          {
+            $record_count++;
+            $productDate[$record_count] = $splitDate[1];
+            $productYear[$record_count] = $splitDate[0];
+            $productAreaType[$record_count] = $master_fields[1].": ".$master_fields[3];
+            $productAreaType[$record_count] =~ s/,//;
+            $productValue[$record_count] = $master_fields[10];
+          }
+        }
+      }
+    }
+    else
+    {
+      warn "Line/record could not be parsed: $records[$record_count]\n";
+    }
+  }
+  open (my $fh, ">priceIndex1.csv")
+    or die "$!";
+  for my $j (1..$record_count)
+  {
+    print $fh "$productDate[$j]".","."$productYear[$j]".","."$productAreaType[$j]".","."$productValue[$j]"."\n";
+  }
+  close ($fh);
+}
+elsif($questionType == 2)
+{
+    my @productYear;
+    my @productArea;
+    my @productValue;
+    my $record_count = 0;
+    foreach my $cost_record (@records)
+    {
+        if ( $csv->parse($cost_record))
+        {
+            my @master_fields = $csv->fields();
+            my @splitDate = split('-', $master_fields[0]);
+            if ($splitDate[0] > 1990 && $splitDate[0] < 2017)
+            {
+                if ($master_fields[3] eq "All-items")
+                {
+                    if ($master_fields[1] eq "Toronto, Ontario" || $master_fields[1] eq "Vancouver, British Columbia" ||$master_fields[1] eq "Halifax, Nova Scotia" ||$master_fields[1] eq "Calgary, Alberta" ||$master_fields[1] eq "Whitehorse, Yukon" ||$master_fields[1] eq "Yellowknife, Northwest Territories")
+                    {
+                        $record_count++;
+                        $productYear[$record_count] = $splitDate[0];
+                        my @splitArea = split(',', $master_fields[1]);
+                        $productArea[$record_count] = $splitArea[0];
+                        $productValue[$record_count] = $master_fields[10];
+                    }
+                }
+            }
+        }
+        else
+        {
+            warn "Line/record could not be parsed: $records[$record_count]\n";
+        }
+    }
+    open (my $fh, ">priceIndex2.csv")
+    or die "$!";
+    for my $j (1..$record_count)
+    {
+        print $fh "$productYear[$j]".","."$productArea[$j]".","."$productValue[$j]"."\n";
+    }
+    close ($fh);
+}
+#    my @location;
+#    my @areaPopulation;
+#    foreach my $census_record ( @records ) {
+#        if( $csv->parse($census_record)) {
+#            my @master_fields = $csv->fields();
+#            if ($master_fields[9] eq "Population, 2016"){
+#                $record_count++;
+#                $location[$record_count] = $master_fields[3];
+#                $location[$record_count] =~ s/,//;
+#                $areaPopulation[$record_count] = $master_fields[12];
+#            }
+#        }
+#        else {
+#        }
+#    }
+#    open (my $fh, ">censusProcessed.csv") or die "$!";
+#    for my $i (1..$record_count) {
+#        print $fh "$location[$i]".","."$areaPopulation[$i]"."\n";
+#    }
+#    close($fh);
+#    foreach my $cost_record ( @records ) {
+#        if ( $csv->parse($cost_record) ) {
+#            my @master_fields = $csv->fields();
+#            if ($master_fields[1] eq "Canada" | $master_fields[1] eq "Newfoundland and Labrador" | $master_fields[1] eq "Prince Edward Island" | $master_fields[1] eq "Nova Scotia" | $master_fields[1] eq "New Brunswick" | $master_fields[1] eq "Quebec" | $master_fields[1] eq "Ontario" | $master_fields[1] eq "Manitoba" | $master_fields[1] eq "Saskatchewan" | $master_fields[1] eq "Alberta" | $master_fields[1] eq "British Columbia")
+#            {
+#                if ($master_fields[3] eq "Alcoholic beverages" | $master_fields[3] eq "Education and reading")
+#                {
+#                    my @splitDate = split('-', $master_fields[0]);
+#                  $productDate[$record_count] = $splitDate[1];
+#                  $productYear[$record_count] = $splitDate[0];
+#                    $productArea[$record_count] = $master_fields[1].": ".$master_fields[3];
+#                    $productValue[$record_count] = $master_fields[9];
+#                    $productArea[$record_count] =~ s/,//;
+#                }
+#            }
+#        }
+#        else
+#        {
+#                warn "Line/record could not be parsed: $records[$record_count]\n";
+#        }
+#    }
+#    open (my $fh, ">priceIndex1.csv") or die "$!";
+#    for my $j (1..$record_count) {
+#        print $fh "$productDate[$j]".","."$productYear[$j]".","."$productArea[$j]".","."$productValue[$j]"."\n";
+#    }
+#    close ($fh);
+#}
+elsif($questionType == 3)
+{
+  my @productYear;
+  my @productType;
+  my @productValue;
+  my $record_count = 0;
+  foreach my $cost_record (@records)
+  {
+    if ( $csv->parse($cost_record))
+    {
+      my @master_fields = $csv->fields();
+      my @splitDate = split('-', $master_fields[0]);
+      if ($splitDate[0] > 1969 && $splitDate[0] < 2019)
+      {
+        if ($master_fields[3] eq "Rented accommodation" | $master_fields[3] eq "Owned accommodation")
+        {
+          if ($master_fields[1] eq "Canada")
+          {
+            $record_count++;
+            $productYear[$record_count] = $splitDate[0];
+            $productType[$record_count] = $master_fields[3];
+            $productValue[$record_count] = $master_fields[10];
+          }
+        }
+      }
+    }
+    else
+    {
+      warn "Line/record could not be parsed: $records[$record_count]\n";
+    }
+  }
+  open (my $fh, ">priceIndex3.csv")
+    or die "$!";
+  for my $j (1..$record_count)
+  {
+    print $fh "$productYear[$j]".","."$productType[$j]".","."$productValue[$j]"."\n";
+  }
+  close ($fh);
+}
+elsif($questionType == 4)
+{
+  my @productYear;
+  my @productType;
+  my @productArea;
+  my @productValue;
+  my $userRequested = 0;
+  my $userString;
+  my $record_count = 0;
+  while ($userRequested < 1 || $userRequested > 16)
+  {
+	   print "What product category would you like you process for?\n1. Food\n2. Household cleaning products\n3. Paper, plastic and aluminum foil supplies\n4. Pet foods and supplies\n5. Seeds, plants and cut flowers\n6. Household furnishing and equipment\n7. Cooking appliances\n8. Clothing and Footwear\n9. Purchase and leasing of passenger vehicles\n10. Health and personal care\n11. Sporting and exercise equipment\n12. Toys, games, and hobby supplies\n13. Photographic equipment and supplies\n14. Home entertainment equipment, parts and services\n15. Video and audio subscription services\n16. Reading material\n";
+    chomp($userRequested = <STDIN>);
+  }
+  if ($userRequested == 1)
+  {
+    $userString = "Food";
+  }
+  elsif ($userRequested == 2)
+  {
+    $userString = "Household cleaning products";
+  }
+  elsif ($userRequested == 3)
+  {
+    $userString = "Paper, plastic and aluminum foil supplies";
+  }
+  elsif ($userRequested == 4)
+  {
+    $userString = "Pet food and supplies";
+  }
+  elsif ($userRequested == 5)
+  {
+    $userString = "Seeds, plants and cut flowers";
+  }
+  elsif ($userRequested == 6)
+  {
+    $userString = "Household furnishings and equipment";
+  }
+  elsif ($userRequested == 7)
+  {
+    $userString = "Cooking appliances";
+  }
+  elsif ($userRequested == 8)
+  {
+    $userString = "Clothing and footwear";
+  }
+  elsif ($userRequested == 9)
+  {
+    $userString = "Purchase and leasing of passenger vehicles";
+  }
+  elsif ($userRequested == 10)
+  {
+    $userString = "Health and personal care";
+  }
+  elsif ($userRequested == 11)
+  {
+    $userString = "Sporting and exercise equipment";
+  }
+  elsif ($userRequested == 12)
+  {
+    $userString = "Toys, games (excluding video games) and hobby supplies";
+  }
+  elsif ($userRequested == 13)
+  {
+    $userString = "Photographic equipment and supplies";
+  }
+  elsif ($userRequested == 14)
+  {
+    $userString = "Home entertainment equipment, parts and services";
+  }
+  elsif ($userRequested == 15)
+  {
+    $userString = "Video and audio subscription services";
+  }
+  elsif ($userRequested == 16)
+  {
+    $userString = "Reading material (excluding textbooks)";
+  }
+  foreach my $cost_record (@records)
+  {
+    if ( $csv->parse($cost_record))
+    {
+      my @master_fields = $csv->fields();
+      my @splitDate = split('-', $master_fields[0]);
+      if ($splitDate[0] > 1987 && $splitDate[0] < 2019)
+      {
+        if ($master_fields[1] eq "Canada" | $master_fields[1] eq "Newfoundland and Labrador" | $master_fields[1] eq "Prince Edward Island" | $master_fields[1] eq "Nova Scotia" | $master_fields[1] eq "New Brunswick" | $master_fields[1] eq "Quebec" | $master_fields[1] eq "Ontario" | $master_fields[1] eq "Manitoba" | $master_fields[1] eq "Saskatchewan" | $master_fields[1] eq "Alberta" | $master_fields[1] eq "British Columbia")
+        {
+          if ($master_fields[3] eq $userString)
+          {
+            $record_count++;
+            $productYear[$record_count] = $splitDate[0];
+            $productArea[$record_count] = $master_fields[1];
+            $productType[$record_count] = $master_fields[3];
+            $productType[$record_count] =~ s/,//;
+            $productValue[$record_count] = $master_fields[10];
+          }
+        }
+      }
+    }
+    else
+    {
+      warn "Line/record could not be parsed: $records[$record_count]\n";
+    }
+  }
+  open (my $fh, ">priceIndex4-".$userRequested.".csv")
+    or die "$!";
+  for my $j (1..$record_count)
+  {
+    print $fh "$productYear[$j]".","."$productArea[$j]".","."$productType[$j]".","."$productValue[$j]"."\n";
+  }
+  close ($fh);
+}
+#
+#   End of Script
+#
---
+++ Final-Karachi-master/src/main.pl
@@ -0,0 +1,127 @@
+#!/usr/bin/perl
+#
+#   Packages and modules;
+#
+#require dataFormat;
+use strict;
+use warnings;
+use version;    our $VERSION = qv('5.16.0');
+use Text::CSV 1.32; #to parse lines
+use File::Basename qw(dirname);
+use Cwd qw(abs_path);
+use lib dirname(dirname abs_path $0) .'/lib';
+use Rsorter qw(graph1 graph2 graph3 graph4);
+#
+# main.pl
+#
+#   Authors: Alex Jetleb (1042992), Jacob Serafin(1071173), Jacob Beal(105704)
+#
+#   Project: Answers Demo
+#   Date of Last Update: March 25, 2019
+#
+#   Functional Summary
+#   main.pl takes in data from the consumer price index to answer the following question:
+#   Over the course of an average year, how have month-to-month trends affected the cost of
+#   education and alcohol per province? What correlations can we find between the cost of alcohol
+#   And the cost of education in an average month?
+#
+#   Statistics data from:
+#
+
+#
+#   Variables to be used
+#
+my @productName; #this holds all the product names
+my @averageCost; #this holds all the CPI values (balanced around 1)
+my @provinceName; #this holds all the respective province names
+my @monthType; #this holds all the months of the year
+
+print "Select which Question you would like to have answered:\n1.   Over the course of an average year, how have month-to-month trends affected the cost of education and alcohol per province?\n2. In a given area, how have the costs 'traditional' communications systems changed in contrast to the costs of 'new' communications systems when compared to the number of people living there?\n3.   Between two given years, what is the average rate of change between the cost of owning and renting a home?\n4.  How does any individual Province's Consumer Price Index for a specific category compare to the Provincial average?\n";
+my $response;
+$response  = 0;
+do
+{
+	print "Enter a number between 1 and 4.\n";
+    chomp ($response = <STDIN>);
+} while ($response < 1 || $response > 4);
+if ($response == 1)
+{
+    print "This program takes in data from the Consumer Price Index and outputs the average cost of alcohol per month against the average cost of education per month.\n";
+	my $filename = '../lib/QuestionOne/priceIndex1.csv'; #look for the correct filename
+	if (-e $filename)
+    {
+		graph1();
+	}
+	else
+	{
+		print "Couldn't find the file - try preprocessing for question 1 in the preprocessing folder, and then dropping the produced file 'priceindex1.csv' into the lib folder";
+	}
+}
+elsif ($response == 2)
+{
+    print "This program takes in data from the Consumer Price Index and the Canadian Census and outputs the change in cost of all products against change in population in Toronto, Vancouver, Halifax, Calgary, Yelloknife, and Whitehorse.\n";
+	my $filename = '../lib/QuestionTwo/priceIndex2.csv'; #look for the correct filename
+	#my $filename2 = '../lib/QuestionTwo/censusData.csv';
+	if (-e $filename) #& -e $filename2) #check if the file exists
+    {
+		graph2();
+	}
+	else
+	{
+		print "Couldn't find the file - try preprocessing for question 2 in the preprocessing folder, and then dropping the produced files 'priceindex2.csv' and 'census.csv' into the lib folder";
+	}
+}
+elsif ($response == 3)
+{
+  print "This program takes in data from the Consumer Price Index, as well as a start and year end entered by you, and outputs the average rate of change between the cost of owning and renting a home in Canada.\n";
+	print "Which year would you like to start at?\n";
+	my $startYear = 0;
+	do
+	{
+		print "Enter any year between 1970 and 2017.\n";
+		chomp ($startYear = <STDIN>);
+	} while ($startYear < 1970 || $startYear > 2017);
+	print "Which year would you like to end at?\n";
+	my $endYear = 0;
+	do
+	{
+		print "Enter any year between your start year of ".$startYear." and 2018.\n";
+		chomp ($endYear = <STDIN>);
+	} while ($endYear <= $startYear || $startYear > 2018);
+
+	my $filename = '../lib/QuestionThree/priceIndex3.csv'; #look for the correct filename
+	if (-e $filename) #check if the file exists
+  {
+		graph3($startYear,$endYear);
+	}
+	else
+	{
+		print "Couldn't find the file - try preprocessing for question 3 in the preprocessing folder, and then dropping the produced file 'priceindex3.csv' into the lib folder";
+	}
+}
+elsif ($response == 4)
+{
+  print "This program takes in data from the Consumer Price Index, as well as a product category and Province entered by you, and outputs the Price Index value for that category in that province compared to the national average over the period lasting from 1988-2018.\n";
+	print "What product category would you like you check?\n1. Food\n2. Household cleaning products\n3. Paper, plastic and aluminum foil supplies\n4. Pet foods and supplies\n5. Seeds, plants and cut flowers\n6. Household furnishing and equipment\n7. Cooking appliances\n8. Clothing and Footwear\n9. Purchase and leasing of passenger vehicles\n10. Health and personal care\n11. Sporting and exercise equipment\n12. Toys, games, and hobby supplies\n13. Photographic equipment and supplies\n14. Home entertainment equipment, parts and services\n15. Video and audio subscription services\n16. Reading material\n";
+	do
+	{
+		print "Enter a number between 1 and 16.\n";
+		chomp ($response = <STDIN>);
+	} while ($response < 1 || $response > 16);
+	my $provinceChoice;
+	print "What province would you like to compare?\n";
+	do
+	{
+		print "1. British Columbia\n2. Alberta\n3. Saskatchewan\n4. Manitoba\n5. Ontario\n6. Quebec\n7. Newfoundland and Labrador\n8. Nova Scotia\n9. New Brunswick\n10. Prince Edward Island\n";
+		chomp ($provinceChoice = <STDIN>);
+	} while ($provinceChoice < 1 || $provinceChoice > 10);
+	my $filename = '../lib/QuestionFour/priceIndex4-'.$response.".csv"; #look for the correct filename - include the product type
+	if (-e $filename) #check if the file exists
+  {
+		graph4($response,$provinceChoice);
+	}
+	else
+	{
+		print "Couldn't find the file - try preprocessing for question 4 in the preprocessing folder with your chosen product, and then dropping the produced file 'priceindex4-x.csv' into the lib folder";
+	}
+}
